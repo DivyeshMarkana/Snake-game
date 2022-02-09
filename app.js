d5f6fe1399c4fc74a2a5c3df840c7b551c865f04
Divyesh Markana <divyeshmarkana724@gmail.com>
2022-02-09 16:31:14 +0530

    make clear canvas for remove previous position

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ let ctx = canvas.getContext("2d");
 
 let snake = [{ x: 150, y: 150 }, { x: 140, y: 150 }, { x: 130, y: 150 }, { x: 120, y: 150 }, { x: 110, y: 150 }];
 
-let dx = 10
+let dx = 10;
+let dy = 0;
 
 function drawSnakePart(snakePart) {
     ctx.fillStyle = "lightblue";
@@ -13,13 +14,27 @@ function drawSnakePart(snakePart) {
     ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
 }
 
-(function drawSnake() {
+function drawSnake() {
     snake.forEach(drawSnakePart);
-})();
+}
 
 function advanceSnake() {
-    const head = {x: snake[0].x + dx, y: snake[0].y};
+    const head = { x: snake[0].x + dx, y: snake[0].y + dy };
     snake.unshift(head);
     snake.pop();
 }
 
+function clearCanvas() {
+    ctx.fillStyle = "white";
+    ctx.strokeStyle = "black";
+    ctx.fillRect(0, 0, canvas.widht, canvas.height);
+    ctx.strokeRect(0, 0, canvas.width, canvas, height)
+}
+
+setTimeout(function onTick() {
+    clearCanvas();
+    drawSnake();
+    advanceSnake();
+}, 100)
+
+drawSnake()
